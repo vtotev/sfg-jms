72927e3f025ce21159cfdf0f9086d7461c84f5c9
Valeri Totev <valerivt@yahoo.com>
2024-01-16 10:01:29 +0200

    embedded server configuration setup

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -5713399837357946761L;
+
+    private UUID id;
+    private String message;
+
+}
